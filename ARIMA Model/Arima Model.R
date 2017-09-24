a37772d9720c6e960e5b6b9ecd45e6e0c52ff751
bp19988 <bryan.a.pham@gmail.com>
2017-09-24 16:09:24 -0400

    Arima Modeling stuff

---
+++ ARIMA Model/Arima Model.R
@@ -0,0 +1,22 @@
+library(tidyquant)
+library(astsa)
+
+
+SPX <- getSymbols("^GSPC",auto.assign = FALSE, from = "2015-01-01")
+SPX <- dailyReturn(SPX)
+SPXret <- SPX$GSPC.Close
+
+SPXts <-as.ts(SPXret, start= c(2015, 1), frequency = 365)
+
+acf2(SPXret, max.lag = 12)
+sarima(SPXret, 0, 1, 1)
+sarima.for(SPXret, n.ahead = 1, 0, 1, 1)
+
+library(quantmod)
+library(astsa)   
+
+SPX <- getSymbols("^GSPC", auto.assign = FALSE, from = "2010-01-01")
+SPXret <- SPX$GSPC.Close
+SPXret <- ts(SPXret, frequency=365, start=c(2010,4))
+sarima.for(SPXret, 5, 1, 1, 3)
+
